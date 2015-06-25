0e6f1deaece0b8c4603c9929683a187962b75645
Eldar Guseynov <elparam@gmail.com>
2015-06-25 16:01:38 +0300

    Add New Sparkland Project

---
+++ Sparkland/Sparkland/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public Sprite PressedSprite;
+
+    private SpriteRenderer _spriteRenderer;
+    private Sprite _nonPressedSprite;
+
+	void Start () {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+	    _nonPressedSprite = _spriteRenderer.sprite;
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        _spriteRenderer.sprite = PressedSprite;
+    }
+
+    void OnMouseUp()
+    {
+        _spriteRenderer.sprite = _nonPressedSprite;
+    }
+
+    void OnMouseUpAsButton()
+    {
+        
+    }
+}
---
+++ Sparkland/Sparkland/Assets/Scripts/PulseColor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PulseColor : MonoBehaviour
+{
+    public Color StartColor = Color.white;
+    public Color EndColor = Color.white;
+    public float ColorTolerance = 0.1f;
+    public float Speed = 5f;
+
+    private SpriteRenderer _spriteRenderer;
+    private Color _endColor;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = StartColor;
+        _endColor = EndColor;
+    }
+
+    private bool IsColorShouldAnimate()
+    {
+        var dA = Mathf.Abs(_spriteRenderer.color.a - _endColor.a);
+        var dB = Mathf.Abs(_spriteRenderer.color.b - _endColor.b);
+        var dR = Mathf.Abs(_spriteRenderer.color.r - _endColor.r);
+        var dG = Mathf.Abs(_spriteRenderer.color.g - _endColor.g);
+        return dA + dG + dB + dR > ColorTolerance;
+    }
+
+    void Update()
+    {
+        if (IsColorShouldAnimate())
+        {
+            _spriteRenderer.color = Color.Lerp(_spriteRenderer.color, _endColor, Time.deltaTime * Speed);
+        }
+        else
+        {
+            _endColor = _endColor == EndColor ? StartColor : EndColor;
+        }
+    }
+}
